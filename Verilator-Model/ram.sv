6ace4640ae677a6c1b01f9df4f1355e0b1c0b524
Janet30-rapha <janet3042000@gmail.com>
2025-07-24 14:50:39 +0530

    Update ram.sv

--- Verilator-Model/ram.sv
+++ Verilator-Model/ram.sv
@@ -1,18 +1,3 @@
-// Copyright 2017 Embecosm Limited <www.embecosm.com>
-// Copyright and related rights are licensed under the Solderpad Hardware
-// License, Version 0.51 (the “License”); you may not use this file except in
-// compliance with the License.  You may obtain a copy of the License at
-// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
-// or agreed to in writing, software, hardware and materials distributed under
-// this License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
-
-// RAM wrapper for RI5CY
-// Contributor: Jeremy Bennett <jeremy.bennett@embecosm.com>
-//
-// This maps the dp_ram module to the instruction and data ports of the RI5CY
-// processor core.
 
 module ram
     #(
