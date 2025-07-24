08655ef95c45fdc30028b3c01b84772a8483ba2d
Janet30-rapha <janet3042000@gmail.com>
2025-07-24 14:23:13 +0530

    Update riscv_apu_disp.sv

--- riscv_apu_disp.sv
+++ riscv_apu_disp.sv
@@ -1,26 +1,4 @@
-// Copyright 2017 ETH Zurich and University of Bologna.
-// Copyright and related rights are licensed under the Solderpad Hardware
-// License, Version 0.51 (the “License”); you may not use this file except in
-// compliance with the License.  You may obtain a copy of the License at
-// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
-// or agreed to in writing, software, hardware and materials distributed under
-// this License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
 
-////////////////////////////////////////////////////////////////////////////////
-// Engineer:       Lukas Mueller - lukasmue@student.ethz.ch                   //
-//                                                                            //
-// Additional contributions by:                                               //
-//                 Michael Gautschi - gautschi@iis.ee.ethz.ch                 //
-//                                                                            //
-// Design Name:    Simple APU dispatcher                                      //
-// Project Name:   PULP                                                       //
-// Language:       SystemVerilog                                              //
-//                                                                            //
-// Description:    Dispatcher for sending instructions to the Marx            //
-//                 interconnect.                                              //
-////////////////////////////////////////////////////////////////////////////////
 
 `include "apu_macros.sv"
 
