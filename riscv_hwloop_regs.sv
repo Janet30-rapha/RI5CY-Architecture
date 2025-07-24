88d94770e9db355c5049ac00667bacd7c2adffb0
Janet30-rapha <janet3042000@gmail.com>
2025-07-24 14:27:23 +0530

    Update riscv_hwloop_regs.sv

--- riscv_hwloop_regs.sv
+++ riscv_hwloop_regs.sv
@@ -1,26 +1,4 @@
-// Copyright 2017 ETH Zurich and University of Bologna.
-// Copyright and related rights are licensed under the Solderpad Hardware
-// License, Version 0.51 (the “License”); you may not use this file except in
-// compliance with the License.  You may obtain a copy of the License at
-// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
-// or agreed to in writing, software, hardware and materials distributed under
-// this License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
 
-////////////////////////////////////////////////////////////////////////////////
-// Engineer:       Michael Gautschi - gautschi@iis.ee.ethz.ch                 //
-//                                                                            //
-// Design Name:    hwloop regs                                                //
-// Project Name:   RI5CY                                                      //
-// Language:       SystemVerilog                                              //
-//                                                                            //
-// Description:    Hardware loop registers                                    //
-//                 a) store start/end address of N=4 hardware loops           //
-//                 b) store init value of counter for each hardware loop      //
-//                 c) decrement counter if hwloop taken                       //
-//                                                                            //
-////////////////////////////////////////////////////////////////////////////////
 
 module riscv_hwloop_regs
 #(
