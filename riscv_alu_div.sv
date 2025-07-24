459225b870fecab99ae34ed0c4f040195fd1ba0f
Janet30-rapha <janet3042000@gmail.com>
2025-07-24 14:21:42 +0530

    Update riscv_alu_div.sv

--- riscv_alu_div.sv
+++ riscv_alu_div.sv
@@ -1,27 +1,4 @@
-// Copyright 2017 ETH Zurich and University of Bologna.
-// Copyright and related rights are licensed under the Solderpad Hardware
-// License, Version 0.51 (the “License”); you may not use this file except in
-// compliance with the License.  You may obtain a copy of the License at
-// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
-// or agreed to in writing, software, hardware and materials distributed under
-// this License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
 
-///////////////////////////////////////////////////////////////////////////////
-// File       : Simple Serial Divider
-// Ver        : 1.0
-// Date       : 15.03.2016
-///////////////////////////////////////////////////////////////////////////////
-//
-// Description: this is a simple serial divider for signed integers (int32).
-//
-///////////////////////////////////////////////////////////////////////////////
-//
-// Authors    : Michael Schaffner (schaffner@iis.ee.ethz.ch)
-//              Andreas Traber    (atraber@iis.ee.ethz.ch)
-//
-///////////////////////////////////////////////////////////////////////////////
 
 module riscv_alu_div
 #(
